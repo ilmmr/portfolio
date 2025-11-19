9fa5fdfd19e85026b0d20c72814ab2735ef2d6dd
Luís Ribeiro <56344898+ilmmr@users.noreply.github.com>
2025-11-19 18:55:43 +0000

    ( ͡° ͜ʖ ͡° )
    Removed the Curriculum Vitae link and button from the home component.

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -70,9 +70,6 @@ export default function Home({darkMode}) {
               borderRadius={'50%'} p={'0.75rem'}/>
          <h3>{info.firstName} {info.lastName}</h3>
          <h1>{info.position}</h1>
-         <a href={CV} target="blank">
-            <button className={classNames(Style.shadowed)}>Curriculum Vitae</button>
-         </a>
          <Typography fontStyle="italic" color="#777777">&#34;Coding is to programming what typing is to writing.&#34;</Typography>
          <Box component={'div'} display={'flex'} gap={'1.5rem'} justifyContent={'center'} fontSize='2rem'>
                {info.socials.map((social, index) => (
